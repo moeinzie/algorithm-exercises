6dcc0d77b7bb5de0e00203af6ad9fe88d8bcbdd9
Moein Mortazavi <moeinzie@gmail.com>
2024-08-06 14:14:32 +0330

    Implement findLongestSubstring function to find the length of the longest substring with all distinct characters

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -1,18 +1,36 @@
-/** 
-Sliding Window - findLongestSubstring
-Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+/**
+ Sliding Window - findLongestSubstring
+ Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
 
-findLongestSubstring(*') // 0
-findLongestSubstring('rithmschool') // 7
-findLongestSubstring('thisisawesome') // 6]
-findLongestSubstring('thecatinthehat') // 7
-findLongestSubstring('bbbbbb') // 1
-findLongestSubstring('longestsubstring') // 8|
-findLongestSubstring('thisishowwedoit') // 6
+ findLongestSubstring(*') // 0
+ findLongestSubstring('rithmschool') // 7
+ findLongestSubstring('thisisawesome') // 6]
+ findLongestSubstring('thecatinthehat') // 7
+ findLongestSubstring('bbbbbb') // 1
+ findLongestSubstring('longestsubstring') // 8|
+ findLongestSubstring('thisishowwedoit') // 6
 
-Time Complexity - O(n)
-**/
+ Time Complexity - O(n)
+ **/
 
-function findLongestSubstring(){
-  // add whatever parameters you deem necessary - good luck!
+function findLongestSubstring(str) {
+
+    let charIndexMap = {};
+    let start = 0;
+    let maxLength = 0;
+
+    for (let end = 0; end < str.length; end++) {
+        let char = str[end];
+
+        if (charIndexMap.hasOwnProperty(char)) {
+            start = Math.max(start, charIndexMap[char] + 1);
+        }
+        charIndexMap[char] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
 }
+
+console.log(findLongestSubstring("abcabcbb"));
+console.log(findLongestSubstring("bbbbb"));
