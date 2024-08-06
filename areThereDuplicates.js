27a8394b222b04ec878b3241d26f44340f2c486c
Moein Mortazavi <moeinzie@gmail.com>
2024-08-06 12:02:51 +0330

    Implement areThereDuplicates function with two approaches
    - Added areThereDuplicates function to check for duplicates among arguments.
    - Implemented a version using the frequency counter pattern with O(n) time and space complexity.
    - Commented out the frequency counter version for reference.
    - Added a bonus version using sorting and multiple pointers with O(n log n) time and O(1) space complexity.
    - Included test case to verify functionality.

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -1,18 +1,51 @@
 /**
-Frequency Counter / Multiple Pointers - areThereDuplicates
-Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
-Examples:
-areThereDuplicates(1, 2, 3) // false areThereDuplicates(1, 2, 2) // true areThereDuplicates('a', 'b', 'c', 'a') // true
+ Frequency Counter / Multiple Pointers - areThereDuplicates
+ Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in. You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+ Examples:
+ areThereDuplicates(1, 2, 3) // false areThereDuplicates(1, 2, 2) // true areThereDuplicates('a', 'b', 'c', 'a') // true
 
-Restrictions:
-Time - O(n)
-Space - O(n)
+ Restrictions:
+ Time - O(n)
+ Space - O(n)
+ **/
 
-Bonus:
-Time - O(n log n)
-Space - O(1)
-**/
 
-function areThereDuplicates() {
-  // good luck. (supply any arguments you deem necessary.)
+// function areThereDuplicates(...args) {
+//     let frequencyCounter = {};
+//
+//     for (let val of args) {
+//         let key = typeof val + val;
+//         if (frequencyCounter[key]) {
+//             return true;
+//         }
+//         frequencyCounter[key] = 1;
+//     }
+//
+//     return false;
+// }
+
+/**
+ Bonus:
+ Time - O(n log n)
+ Space - O(1)
+ **/
+function areThereDuplicates(...args) {
+
+    args.sort((a, b) => {
+        if (typeof a !== typeof b) {
+            return typeof a > typeof b ? 1 : -1;
+        }
+        if (a > b) return 1;
+        if (a < b) return -1;
+        return 0;
+    });
+
+    for (let i = 0; i < args.length - 1; i++) {
+        if (args[i] === args[i + 1]) {
+            return true;
+        }
+    }
+    return false
 }
+
+console.log(areThereDuplicates('a', 'b', 'c', 2, 4, 'a')); // true
